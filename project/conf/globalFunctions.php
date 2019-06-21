972042741c9512bc2e0a22eefbd83ac0d3bd8b8e
harika91 <harika91@users.noreply.github.com>
2019-06-21 15:27:32 -0500

    Update globalFunctions.php

--- project/conf/globalFunctions.php
+++ project/conf/globalFunctions.php
@@ -429,8 +429,8 @@ $tokenURL = 'https://github.com/login/oauth/access_token';
                 'client_secret' => 'cdb0f0faee2f2f9ea3e552e5997d3d5ea3ffbcb4',
                 'redirect_uri' => 'https://dev.rgd.mcw.edu/rgdCuration/',
                 'code' => $_GET['code'],
-                'Accept' => 'application/vnd.github.v3+json',
-                'User-Agent' => 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 YaBrowser/16.3.0.7146 Yowser/2.5 Safari/537.36' );
+                'Accept' => 'application/json'
+                );
 
        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
        $response = curl_exec($ch);
