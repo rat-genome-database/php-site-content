91642a633b05470b3da153a8b0ffefac12dad686
harika91 <harika91@users.noreply.github.com>
2019-06-24 10:20:49 -0500

    Update globalFunctions.php

--- project/conf/globalFunctions.php
+++ project/conf/globalFunctions.php
@@ -438,11 +438,12 @@ function userLoggedIn()
        $token = $accesstoken->access_token;
 */
 
-   $jsonData = json_decode(file_get_contents('https://github.com/login/oauth/access_token?client_id=ee483d03b1806882b4b2&client_secret=cdb0f0faee2f2f9ea3e552e5997d3d5ea3ffbcb4&redirect_uri=https://dev.rgd.mcw.edu/rgdCuration/&code='.$_GET['code']));
+$url = 'https://github.com/login/oauth/access_token?client_id=ee483d03b1806882b4b2&client_secret=cdb0f0faee2f2f9ea3e552e5997d3d5ea3ffbcb4&redirect_uri=https://dev.rgd.mcw.edu/rgdCuration/&code='.$_GET['code'];
+   $jsonData = json_decode(file_get_contents($url));
 
 
 
-        setSessionVar('uid', $jsonData);
+        setSessionVar('uid', $url);
                        setCookieVar('userloggedin', '1');
                        setSessionVar('userGroup', 'admin');
                        setSessionVar('userFullName', $jsonData->access_token);
