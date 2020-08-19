15190b82d59c4281efab5e09a5416c3959d6ef43
jt15 <jthota01@gmail.com>
2020-08-18 22:24:53 -0500

    updated

--- project/modules/curation.php
+++ project/modules/curation.php
@@ -1157,36 +1157,25 @@ function curation_selectTerms() {
 		if ($acc_id) preg_match("/[A-Z]+/", $acc_id[0], $ont_id);
 	}
 	
-	$toReturn = '    <script src="/rgdweb/js/jquery/jquery-1.12.4.min.js"></script>
-    <script src="/rgdweb/js/jquery/jquery-ui-1.8.18.custom.min.js"></script>
-    <script src="/rgdweb/js/jquery/jquery_combo_box.js"></script>
-    <!--script src="/rgdweb/js/jquery/jquery-migrate-1.2.0.js"></script-->
-    <script type="text/javascript"  src="js/jquery.autocomplete.curation.js"></script>';
-	$toReturn .= '<script type="text/javascript">$(document).ready(function(){$("#objectName").autocomplete({
-	         source:function(request, response){
-                        $.ajax({
-                            url:"/OntoSolr/select",
-                            data:{
-                                "q":request.term,
-                                //"qf": "term_en^5 term_str^3 term^3 synonym_en^4.5  synonym_str^2 synonym^2 def^1 idl_s^1",
-                                "fq": "cat:(BP CC MF MP HP NBO PW RDO RS VT CMO MMO XCO CHEBI)",
-                                "wt": "velocity",
-                                "bf": "term_len_l^10",
-                                "v.template": "termmatch",
-                                "cacheLength": 0
-                            },
-                            scrollHeight: 240,
-                            max: 40,
-                            success:function(data){
-                            console.log(data);
-                                response(data);
-                            }
-
-                        })
-                    }
-
-                                        });
-                     $("input[name=submitBtn]").hide();
+	$toReturn = '<link rel="stylesheet" href="/QueryBuilder/js/jquery-ui-1.12.1/jquery-ui.css" type="text/css" media="all" />
+                    <script type="text/javascript" src="/QueryBuilder/js/jquery-1.12.4.min.js"></script>
+                    <link rel="stylesheet" type="text/css" href="/QueryBuilder/js/main.css"/>
+                    <link rel="stylesheet" href="/QueryBuilder/css/jquery.autocomplete.css" type="text/css" />
+                    <script type="text/javascript" src="/QueryBuilder/js/jquery-ui-1.12.1/jquery-ui.js"></script>
+                    <script type="text/javascript" src="/QueryBuilder/js/jquery.autocomplete.js"></script>
+    <!--script type="text/javascript"  src="https://ontomate.rgd.mcw.edu/OntoSolr/admin/file?file=/velocity/jquery.autocomplete.curation.js&contentType=text/javascript"></script-->';
+	$toReturn .= '<script type="text/javascript">$(document).ready(function(){$("#objectName").autocomplete("/OntoSolr/select", {extraParams:{
+                                             //"qf": "term_en^5 term_str^3 term^3 synonym_en^4.5  synonym_str^2 synonym^2 def^1 idl_s^1",
+                                             "fq": "cat:(BP CC MF MP HP NBO PW RDO RS VT CMO MMO XCO CHEBI)",
+                                             "wt": "velocity",
+                                             "bf": "term_len_l^10",
+                                             "v.template": "termmatch",
+                                             "cacheLength": 0
+                                           },
+                                           scrollHeight: 240,
+                                           max: 40
+                                         });
+                     $("#objectName").result(function(data, value){$("#form").submit();});$("input[name=submitBtn]").hide();
 			         $("#objectName").focus();';
 	$closeReturn = '});</script> Ontologies: <a href="/rgdCuration/?module=curation&func=selectTerms&objectName=biological_process+(GO%3A0008150)&ontology=&hiddenXYZ123=">BP</a> '
 			.'<a href="/rgdCuration/?objectName=cellular_component+%28GO%3A0005575%29&hiddenXYZ123=&module=curation&func=selectTerms">CC</a> ' 
