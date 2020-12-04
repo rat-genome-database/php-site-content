186a89ac5b3c01a8957ef5633bdfaa3f6d4125f0
Thota <jthota@mcw.edu>
2020-12-04 16:12:26 -0600

    updated

--- project/modules/curation.php
+++ project/modules/curation.php
@@ -1165,10 +1165,10 @@ function curation_selectTerms() {
 
     <!--script type="text/javascript"  src="https://ontomate.rgd.mcw.edu/OntoSolr/admin/file?file=/velocity/jquery.autocomplete.curation.js&contentType=text/javascript"></script-->';
 	$toReturn .= '<script type="text/javascript">$(document).ready(function(){$("#objectName").autocomplete("/OntoSolr/select", {extraParams:{
-                                             "qf": "term",
+                                             "qf": "term_en^5 term_str^3 term^3 synonym_en^4.5  synonym_str^2 synonym^2 def^1 idl_s^1",
                                              "fq": "cat:(BP CC MF MP HP NBO PW RDO RS VT CMO MMO XCO CHEBI)",
                                              "wt": "velocity",
-                                             "bf": "term_len_l^10",
+                                             "bf": "term_len_l^.02",,
                                              "v.template": "termmatch",
                                              "cacheLength": 0
                                            },
