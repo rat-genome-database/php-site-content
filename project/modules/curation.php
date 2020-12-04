d3647503d9e4a284599b09eda850468fe5a08c5d
Thota <jthota@mcw.edu>
2020-12-04 16:16:41 -0600

    updated

--- project/modules/curation.php
+++ project/modules/curation.php
@@ -1168,7 +1168,7 @@ function curation_selectTerms() {
                                              "qf": "term_en^5 term_str^3 term^3 synonym_en^4.5  synonym_str^2 synonym^2 def^1 idl_s^1",
                                              "fq": "cat:(BP CC MF MP HP NBO PW RDO RS VT CMO MMO XCO CHEBI)",
                                              "wt": "velocity",
-                                             "bf": "term_len_l^.02",,
+                                             "bf": "term_len_l^.02",
                                              "v.template": "termmatch",
                                              "cacheLength": 0
                                            },
