7d7fc738f74a92d0ec8fcd177098b8779e9c5e11
Thota <jthota@mcw.edu>
2025-07-03 13:42:47 -0500

    jquery upgrade

--- project/modules/curation.php
+++ project/modules/curation.php
@@ -1177,7 +1177,7 @@ function curation_selectTerms() {
 	$toReturn = '    <script src="/rgdweb/js/jquery/jquery-3.7.1.min.js"></script>
     <script src="/rgdweb/js/jquery/jquery-ui-1.8.18.custom.min.js"></script>
     <script src="/rgdweb/js/jquery/jquery_combo_box.js"></script>
-    <script type="text/javascript" src="/rgdweb/js/jquery/jquery-migrate-3.5.0.js"></script>
+    <script type="text/javascript" src="/rgdweb/js/jquery/jquery-migrate-3.5.0.min.js"></script>
     <script type="text/javascript" src="/QueryBuilder/js/jquery.autocomplete.js"></script>
 
     <!--script type="text/javascript"  src="https://ontomate.rgd.mcw.edu/OntoSolr/admin/file?file=/velocity/jquery.autocomplete.curation.js&contentType=text/javascript"></script-->';
