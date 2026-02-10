4344ddbf80457b6850a0d85e749c23332200e32b
Thota <jthota@mcw.edu>
2026-02-10 14:40:11 -0600

    updated ontomate button

--- project/modules/curation.php
+++ project/modules/curation.php
@@ -1471,7 +1471,7 @@ function curation_selectReferences() {
 	$toReturn .= '<input type="button" onClick="verify()" value="Search in OntoMate">';
 	$toReturn .= '&nbsp;&nbsp;';
 //	if (strpos($_SERVER['HTTP_HOST'], 'dev.') !== false) {
-//		$toReturn .= '<input type="button" onClick="verifyOld()" value="Search in Old OntoMate">';
+		$toReturn .= '<input type="button" onClick="verifyOld()" value="Search in Old OntoMate">';
 //	}
 
 	$toReturn .= '<p><p>';
